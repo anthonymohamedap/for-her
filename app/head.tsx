c295329d1cacdb857d4c7c0a6ba65b5caf86a826
Anthony <anthonymomed@gmail.com>
2026-05-29 18:43:56 +0200

    feat: tulip app icon, PWA manifest, head tags

---
+++ app/head.tsx
@@ -0,0 +1,15 @@
+export default function Head() {
+  return (
+    <>
+      <title>flowers for her</title>
+      <meta name="description" content="a little garden, just for you" />
+      <meta name="theme-color" content="#0a0118" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      <meta name="apple-mobile-web-app-title" content="flowers for her" />
+      <link rel="icon" type="image/png" href="/icon.png" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      <link rel="manifest" href="/manifest.json" />
+    </>
+  )
+}
